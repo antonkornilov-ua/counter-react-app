15dd98ff616eaadba4058b0c6c2f5720daf9957d
antonkornilov-ua <anton.kornilov@hotmail.com>
2022-11-09 20:54:48 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.scss';
+
+const nums = [1, 2, 5, 10];
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState[0];
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const handleAdd = (num) => {
+        setCount(count + num);
+    };
+    const handleDeduct = (num) => {
+        setCount(count - num);
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <h1>
+                    Coount:
+                    <span id="count" className={count > 0 ? 'positive' : count < 0 ? 'negative' : null}>
+                        {count}
+                    </span>
+                </h1>
+            </div>
+            <div>
+                <h4>Increment</h4>
+                {nums.map((num) => (
+                    <button key={num} onClick={() => handleAdd(num)}>
+                        +{num}
+                    </button>
+                ))}
+            </div>
+            <div>
+                <h4>Decrement</h4>
+                {nums.map((num) => (
+                    <button key={num} onClick={() => handleDeduct(num)}>
+                        -{num}
+                    </button>
+                ))}
+            </div>
+            <div>
+                <button onClick={() => setCount(0)} className="reset">
+                    RESET
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
